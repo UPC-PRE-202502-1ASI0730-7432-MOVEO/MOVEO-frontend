01219ce01cbf76beb49e4ceccab744f8d3caa54f
St4rLght05 <toadmaster009@gmail.com>
2025-10-08 01:34:41 -0500

    fix(adventure): fixed adventure store

--- src/app/adventure/application/adventures.store.js
+++ src/app/adventure/application/adventures.store.js
@@ -1,42 +1,43 @@
-import { ref } from 'vue'
-import { AdventuresApi } from '../infrastructure/adventures-api.assembler.js'
-import { AdventureAssembler } from '../infrastructure/adventure.assembler.js'
+import { ref } from "vue";
+import { Adventure } from "../domain/model/adventure.entity.js";
+import { AdventuresApi } from "../infrastructure/adventures-api.assembler.js";
+import { AdventureAssembler } from "../infrastructure/adventure.assembler.js";
 
-const adventureApi = new AdventuresApi()
+const adventureApi = new AdventuresApi();
 
 export default function useAdventuresStore() {
-    const adventures = ref([])
-    const adventuresLoaded = ref(false)
-    const errors = ref([])
+    const adventures = ref([]);
+    const adventuresLoaded = ref(true)
+    const errors = ref([]);
 
-    function fetchAdventures() {
+    async function fetchAdventures() {
         try {
-            const response = adventureApi.getAdventures()
-            adventures.value = AdventureAssembler.toEntitiesFromResponse(response)
-            adventuresLoaded.value = true
+            const response = await adventureApi.getAdventures();
+            adventures.value = AdventureAssembler.toEntitiesFromResponse(response);
+            adventuresLoaded.value = true;
         } catch (error) {
-            errors.value.push(error)
+            errors.value.push(error);
+            console.error("Error fetching adventures:", error);
         }
     }
 
     function getAdventureById(id) {
-        return adventures.value.find(a => a?.id === Number(id))
+        return adventures.value.find(a => a.id === Number(id));
     }
 
     function addAdventure(adventure) {
-        adventure.id = Date.now()
-        adventures.value.push(adventure)
+        adventure.id = Date.now() // id único temporal
+        adventures.value.push(new Adventure({ ...adventure })) // crea instancia
     }
 
+
     function updateAdventure(updatedAdventure) {
-        const index = adventures.value.findIndex(a => a?.id === updatedAdventure.id)
-        if (index !== -1) {
-            adventures.value[index] = updatedAdventure
-        }
+        const index = adventures.value.findIndex(a => a.id === updatedAdventure.id);
+        if (index !== -1) adventures.value[index] = updatedAdventure;
     }
 
     function deleteAdventure(adventure) {
-        adventures.value = adventures.value.filter(a => a?.id !== adventure.id)
+        adventures.value = adventures.value.filter(a => a.id !== adventure.id);
     }
 
     return {
@@ -48,5 +49,5 @@ export default function useAdventuresStore() {
         addAdventure,
         updateAdventure,
         deleteAdventure
-    }
+    };
 }
